a0027091a299f0d44efbf4c635e606e151f36b91
AbeNalee <abrahamaguvasu@gmail.com>
2022-11-17 18:19:14 +0300

    procfile

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         $this->call(UsersSeeder::class);
+        $this->call(UsersSeeder::class);
         $this->call(ProductsSeeder::class);
     }
 }
