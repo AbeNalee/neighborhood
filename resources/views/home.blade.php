26863a7fa5130c16cff4e06e80ee36d81c361ae5
AbeNalee <abrahamaguvasu@gmail.com>
2022-11-18 20:22:00 +0300

    view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,35 +33,33 @@
         <div class="col-12 col-lg-6 my-1">
             <make-sale purpose="purchase"></make-sale>
         </div>
-        <div class="col-12 col-lg-6 my-1">
-            <div class="card">
-                <div class="card-header">
-                    <span class="">Low Stock (lt 5)</span>
-                    @if(in_array(\Illuminate\Support\Facades\Auth::user()->email, ['abrahamaguvasu@gmail.com', 'muteshiteddy@gmail.com']))
+        @if(in_array(\Illuminate\Support\Facades\Auth::user()->email, ['abrahamaguvasu@gmail.com', 'muteshiteddy@gmail.com']))
+            <div class="col-12 col-lg-6 my-1">
+                <div class="card">
+                    <div class="card-header">
+                        <span class="">Low Stock (lt 5)</span>
                         <button class="btn btn-secondary float-end py-0" data-bs-toggle="modal"
-                            data-bs-target="#updateModal" type="button">Update</button>
-                    @endif
-                </div>
+                                data-bs-target="#updateModal" type="button">Update</button>
+                    </div>
 
-                <div class="card-body">
-                    <ul class="list px-2">
-                        @foreach(\App\Models\Product::lowStock()->get() as $product)
-                            <li class="list-group-item">
-                                <h3 class="form-control">
-                                    {{ $product->name }}
-                                    <span class="fw-bold float-end px-2 rounded {{
+                    <div class="card-body">
+                        <ul class="list px-2">
+                            @foreach(\App\Models\Product::lowStock()->get() as $product)
+                                <li class="list-group-item">
+                                    <h3 class="form-control">
+                                        {{ $product->name }}
+                                        <span class="fw-bold float-end px-2 rounded {{
                                           $product->stock > 2 ? "bg-warning":
                                           "bg-danger" }}">
                                         {{ $product->stock }}
                                     </span>
-                                </h3>
-                            </li>
-                        @endforeach
-                    </ul>
+                                    </h3>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-        @if(in_array(\Illuminate\Support\Facades\Auth::user()->email, ['abrahamaguvasu@gmail.com', 'muteshiteddy@gmail.com']))
             <div class="modal fade" id="updateModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
